ce085fff7a7b57da84d0a5127461fe80ef4435c5
unknown <ahjoubsanae19@gmail.com>
2024-11-22 14:22:43 +0100

    project starting

---
+++ index.c
@@ -0,0 +1,16 @@
+include<stdio.h>
+#include<stdlib.h>
+typedef struct 
+{   char srtruct[50];
+     char description[50];
+     }
+   void afficher(){};
+   void suprimee(){};
+   void modifiee(){};
+   void sauvgardee(){};
+    int main (){
+    
+
+
+return 0;
+} 
